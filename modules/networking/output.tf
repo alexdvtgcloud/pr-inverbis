146b594c157f61f1bf8e0217961daa17b0ae0868
alexdvtgcloud <113016343+alexdvtgcloud@users.noreply.github.com>
2023-09-20 13:41:19 +0200

    Update output.tf

--- modules/networking/output.tf
+++ modules/networking/output.tf
@@ -1,1 +1,19 @@
 
+output "id_network"{
+    description = "ID generated network"
+    value = google_compute_network.vpc_network.id
+}
+output "id_subnet"{
+    description = "ID generated subnet"
+    value = google_compute_subnetwork.subnet.id
+}
+
+output "address_inverbis" {
+  description = "The address of test app"
+  value = google_compute_global_address.inverbis.address
+}
+
+output "private_ip_name" {
+  description = "Name of private IP for Cloud SQL"
+  value = google_compute_global_address.private_ip_address.name
+}
