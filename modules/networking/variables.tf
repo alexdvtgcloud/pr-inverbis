c7b9281f7a375f3ead2234152d4ef30f79ea6484
alexdvtgcloud <113016343+alexdvtgcloud@users.noreply.github.com>
2023-09-20 13:43:16 +0200

    Update variables.tf

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,1 +1,35 @@
+variable "network" {
+  description = "Network name"
+  type = string
+}
 
+variable "project_id" {
+  description = "The ID of the project where this VPC will be created"
+  type        = string
+}
+
+variable "subnet" {
+  description = "Subnet name"
+  type = string
+}
+
+variable "region" {
+  description = "The GCP region"
+  type = string
+}
+
+variable "subnetwork_ipv4_cidr_block" {
+  description = "CIDR of subnet"
+  type = string
+}
+
+variable "env" {
+  description = "Environment"
+  type = string
+}
+
+variable "routing_mode" {
+  type        = string
+  default     = "REGIONAL"
+  description = "The network routing mode (default 'GLOBAL')"
+}
