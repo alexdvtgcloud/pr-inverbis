13868070d6ac3af35f2239b60a2b047e42c6b30b
alexdvtgcloud <113016343+alexdvtgcloud@users.noreply.github.com>
2023-09-20 13:55:03 +0200

    Update main.tf
    cambiar nombre repo

--- modules/artifact-registry/main.tf
+++ modules/artifact-registry/main.tf
@@ -1,1 +1,7 @@
-
+resource "google_artifact_registry_repository" "my-repo" {
+  project       = var.project_id
+  location      = "europe-west1"
+  repository_id = var.repository_name
+  description   = "Test repository"
+  format        = "DOCKER"
+}
